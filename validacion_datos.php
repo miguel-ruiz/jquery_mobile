7ca639c1b49ff2f5cdd042c4c0c6b1c9d7ca59f5
miguel-ruiz <angell_2807@gmail.com>
2015-11-20 16:30:13 -0600

    cambios
    as

--- validacion_datos.php
+++ validacion_datos.php
@@ -13,14 +13,16 @@ $passwordEnviado = $_GET['password'];
 /* crea un array con datos arbitrarios que seran enviados de vuelta a la aplicacion */
 $resultados = array();
 /* verifica que el usuario y password concuerden correctamente */
-if(  $usuarioEnviado == $usuarioValido  && $passwordEnviado == $passwordValido ){
-	/*esta informacion se envia solo si la validacion es correcta */
+/*if($usuarioEnviado == $usuarioValido  && $passwordEnviado == $passwordValido ){
+	/*esta informacion se envia solo si la validacion es correcta *
 	$resultados["mensaje"] = "Validacion Correcta";
 }else{
-	/*esta informacion se envia si la validacion falla */
+	/*esta informacion se envia si la validacion falla *
 	$resultados["mensaje"] = "Usuario y password incorrectos";
 }
+*/
 /*convierte los resultados a formato json*/
+$resultados["mensaje"] =$usuarioEnviado;
 $resultadosJson = json_encode($resultados);
 /*muestra el resultado en un formato que no da problemas de seguridad en browsers */
 echo $_GET['jsoncallback'] . '(' . $resultadosJson . ');';
